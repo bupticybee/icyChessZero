e56225e9c3e92f473f82ccffbc344d65aa2f2476
bupticybee <icybee@bupt.edu.cn>
2018-07-19 08:47:08 +0800

    first version daily update

--- scripts/daily_update.sh
+++ scripts/daily_update.sh
@@ -1,12 +1,10 @@
-cd /work/icybee/icyElephant
+cd /work/icybee/icyChessZero/scripts
 sh kill_all.sh
 sleep 10s
 sh multithread_start.sh -p /usr/local/bin/python3  -g 1 -t 20 -n no
 sleep 10s
-/usr/local/bin/python3 model_update.py
+/usr/local/bin/python3 model_update.py -g 0
 sleep 10s
-sh validate.sh -p /usr/local/bin/python3
+sh validate.sh -p /usr/local/bin/python3 -g 0 -t 20
 sleep 10s
-/usr/local/bin/python3 cal_elo.py
-sleep 10s
-sh multithread_start.sh -p /usr/local/bin/python3  -g 0 
+sh multithread_start.sh -p /usr/local/bin/python3  -g 0 -t 20
