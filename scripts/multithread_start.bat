a3823cc42a813912775d763a66ba82f63dacf4b4
icybee <icybee@yeah.net>
2018-07-25 12:19:46 +0800

    modify bat for win

--- scripts/multithread_start.bat
+++ scripts/multithread_start.bat
@@ -1,5 +1,5 @@
 @echo off
 for /L %%x in (1, 1,%1) do (
-	echo "start /min python self_play.py -g %2 -s %3"
-	start /min python self_play.py -g %2 -s %3
+	echo "start /min %3 icyChess_selfplay.py -g %2"
+	start /min %3 icyChess_selfplay.py -g %2
 )
