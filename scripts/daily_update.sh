40b3c67105e5aa1963db75988eb5d81d6d7a9900
bupticybee <icybee@bupt.edu.cn>
2018-08-27 00:40:31 +0800

    fix bug of weight up

--- scripts/daily_update.sh
+++ scripts/daily_update.sh
@@ -16,7 +16,7 @@ sleep 10s
 sleep 10s
 sh validate.sh -p /usr/local/bin/python3 -g 0 -t 20 > validate_log.txt
 sleep 10s
-python3 check_ifup.py > uplog.tt
+/usr/local/bin/python3 check_ifup.py > uplog.tt
 
 sleep 10s
 sh multithread_start.sh -p /usr/local/bin/python3  -g 0 -t 20
