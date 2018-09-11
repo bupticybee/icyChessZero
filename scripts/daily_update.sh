cb55f4b4b781cbe0e298771edf562426c77f72ec
bupticybee <icybee@bupt.edu.cn>
2018-09-11 22:21:38 +0800

    decay lr to 1e-3

--- scripts/daily_update.sh
+++ scripts/daily_update.sh
@@ -14,9 +14,15 @@ sh multithread_start.sh -p /usr/local/bin/python3  -g 1 -t 20 -n no
 sleep 10s
 /usr/local/bin/python3 model_update.py -g 0 > update_log.txt
 sleep 10s
-sh validate.sh -p /usr/local/bin/python3 -g 0 -t 20 > validate_log.txt
+sh kill_all.sh
+sleep 10s
+sh validate.sh -p /usr/local/bin/python3 -g 0 -t 20 > validate_log.txt &
+sh validate.sh -p /usr/local/bin/python3 -g 1 -t 20 > validate_log.txt 
 sleep 10s
 /usr/local/bin/python3 check_ifup.py > uplog.tt
 
+sh kill_all.sh
+sleep 10s
+sh multithread_start.sh -p /usr/local/bin/python3  -g 1 -t 20 -n no
 sleep 10s
 sh multithread_start.sh -p /usr/local/bin/python3  -g 0 -t 20
