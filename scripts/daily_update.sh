bc2e8feffff1b52cb2b675e686d2f5093568fa29
bupticybee <icybee@bupt.edu.cn>
2018-10-05 11:40:24 +0800

    change lr to 0.01

--- scripts/daily_update.sh
+++ scripts/daily_update.sh
@@ -9,8 +9,8 @@ if [ $count -le 10000 ];then
 fi 
 
 sh kill_all.sh
-sleep 10s
-sh multithread_start.sh -p /usr/local/bin/python3  -g 1 -t 20 -n no
+#sleep 10s
+#sh multithread_start.sh -p /usr/local/bin/python3  -g 1 -t 20 -n no
 sleep 10s
 /usr/local/bin/python3 model_update.py -g 0 > update_log.txt
 sleep 10s
@@ -18,13 +18,13 @@ sh kill_all.sh
 sleep 10s
 /usr/local/bin/python3 upweight.py > uplog.tt
 sleep 10s
-sh validate.sh -p /usr/local/bin/python3 -g 0 -t 20 > validate_log.txt &
-sh validate.sh -p /usr/local/bin/python3 -g 1 -t 20 > validate_log.txt 
+sh validate.sh -p /usr/local/bin/python3 -g 0 -t 20 > validate_log.txt #&
+#sh validate.sh -p /usr/local/bin/python3 -g 1 -t 20 > validate_log.txt 
 sleep 10s
 #/usr/local/bin/python3 check_ifup.py > uplog.tt
 
 sh kill_all.sh
+#sleep 10s
+#sh multithread_start.sh -p /usr/local/bin/python3  -g 1 -t 20 -n no
 sleep 10s
-sh multithread_start.sh -p /usr/local/bin/python3  -g 1 -t 20 -n no
-sleep 10s
-sh multithread_start.sh -p /usr/local/bin/python3  -g 0 -t 20
+sh multithread_start.sh -p /usr/local/bin/python3  -g 0 -t 5
